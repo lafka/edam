b4832233f0fc1905574662e9fbf4e4e70b78e26b
Olav Frengstad <olav@fwt.no>
2013-04-13 23:50:34 +0200

    Add support for arguments

--- src/epm.erl
+++ src/epm.erl
@@ -1,12 +1,51 @@
 -module(epm).
 
 -export([
-%	  repos/1
-%	  dep_exists/2
-%	, dep_check/2
-%	, dep_update/2
-]).
+	  init/0
+	, init/1
+	, get/1
+	, get/2
+	, set/2
+	]).
 
 -include("epm.hrl").
 
+-define(app, application:get_application()).
 
+-spec init() -> ok.
+init() ->
+	init([]).
+
+-spec init([{atom(), term()}]) -> ok.
+init(Args) ->
+	Args2 = lists:ukeymerge(1
+		, lists:ukeysort(1, Args)
+		, lists:ukeysort(1, [
+			  {autofetch, true} % Automatically fetch deps when required
+			, {dryrun, false}   % Determines if side effects are allowed
+			, {verbosity, info} % What messages to display
+			, {basedir, undefined} % The basedir of epm
+		])),
+	lists:foreach(fun({K,V}) -> set(K, V) end, Args2),
+	ok.
+
+-spec get(atom()) -> term().
+get(Key) ->
+	case get(Key, undefined) of
+		undefined ->
+			error(badarg, [Key]);
+		Val ->
+			Val
+	end.
+
+-spec get(atom(), Ret) -> Ret when Ret :: term().
+get(Key, Default) ->
+	case application:get_env(?app, Key) of
+		{ok, Val} -> Val;
+		undefined -> Default
+	end.
+
+-spec set(atom(), Val) -> Val when Val :: term().
+set(Key, Val) ->
+	ok = application:set_env(?app, Key, Val),
+	Val.
